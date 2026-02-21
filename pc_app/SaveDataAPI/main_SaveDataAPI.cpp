72a64d9cd2de2f9eefe8f856ffb39ba2292c4759
silasenapong <sila_sanapong@cmu.ac.th>
2026-02-21 14:28:31 +0700

    add file

--- pc_app/SaveDataAPI/main_SaveDataAPI.cpp
+++ pc_app/SaveDataAPI/main_SaveDataAPI.cpp
@@ -5,9 +5,65 @@
 */
 
 #include <iostream>
+#include <string>
+#include <curl/curl.h>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+size_t WriteCallback(void *contents, size_t size, size_t nmemb, std::string *userp)
+{
+    userp->append((char *)contents, size * nmemb);
+    return size * nmemb;
+}
 
 int main()
 {
+    CURL *curl;
+    CURLcode res;
+    std::string readBuffer;
+
+    curl = curl_easy_init();
+    if (curl)
+    {
+        std::string url = "https://api.openaq.org/v2/latest?city=Bangkok&parameter=pm25";
+
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+
+        curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK)
+        {
+            std::cerr << "CURL Error: " << curl_easy_strerror(res) << std::endl;
+        }
+        else
+        {
+            try
+            {
+                auto data = json::parse(readBuffer);
+
+                if (!data["results"].empty())
+                {
+                    auto measurement = data["results"][0]["measurements"][0];
+                    std::cout << "--- Air Quality Report ---" << std::endl;
+                    std::cout << "Location: Bangkok" << std::endl;
+                    std::cout << "Parameter: " << measurement["parameter"] << std::endl;
+                    std::cout << "Value: " << measurement["value"] << " " << measurement["unit"] << std::endl;
+                    std::cout << "Last Updated: " << measurement["lastUpdated"] << std::endl;
+                }
+            }
+            catch (json::parse_error &e)
+            {
+                std::cerr << "JSON Parse Error: " << e.what() << std::endl;
+            }
+        }
+
+        curl_easy_cleanup(curl);
+    }
 
     return 0;
 }
